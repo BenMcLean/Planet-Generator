a9a4d340ef668f920753f5e8591789baaceb11f0
Benjamin McLean <mclean.ben@gmail.com>
2016-12-01 16:20:29 -0600

    added TEttinger's shader

--- android/assets/shaders/FragmentShader.glsl
+++ android/assets/shaders/FragmentShader.glsl
@@ -1,5 +1,17 @@
-#version 120
+#ifdef GL_ES
+#define LOWP lowp
+precision mediump float;
+#else
+#define LOWP
+#endif
+
+varying LOWP vec4 v_color;
+varying vec2 v_texCoords;
+uniform sampler2D u_texture;
+uniform sampler2D u_texPalette;
 
 void main() {
-
+    vec4 color = texture2D(u_texture, v_texCoords);
+    vec2 index = vec2(color.r, v_color.r);
+    gl_FragColor = vec4(texture2D(u_texPalette, index).rgb, color.a);
 }
--- android/assets/shaders/VertexShader.glsl
+++ android/assets/shaders/VertexShader.glsl
@@ -1,5 +1,13 @@
-#version 120
+attribute vec4 a_position;
+attribute vec4 a_color;
+attribute vec2 a_texCoord0;
+uniform mat4 u_projTrans;
+varying vec4 v_color;
+varying vec2 v_texCoords;
 
 void main() {
-
+    v_color = COLOR_ATTRIBUTE;
+    v_color.a = v_color.a * 1.0039216; //* (256.0/255.0)
+    v_texCoords = TEXCOORD_ATTRIBUTE0;
+    gl_Position = u_projTrans * POSITION_ATTRIBUTE;
 }
