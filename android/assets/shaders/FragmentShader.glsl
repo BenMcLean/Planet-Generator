cee1c7f7f777f7d4c2fd243061df55ad4432bcef
Benjamin McLean <mclean.ben@gmail.com>
2016-12-01 18:02:02 -0600

    tweaking shader, screen still black

--- android/assets/shaders/FragmentShader.glsl
+++ android/assets/shaders/FragmentShader.glsl
@@ -14,4 +14,5 @@ uniform sampler2D u_texPalette;
 void main() {
     vec4 color = texture2D(u_texture, v_texCoords); // gets the color of the pixel that it's drawing; this is the grayscale sprite's color before repaletting
     vec2 index = vec2(color.r, 0); // index refers to where to look in the palette image to find which color to draw
+    gl_FragColor = vec4(texture2D(u_texPalette, index).rgba);
 }
