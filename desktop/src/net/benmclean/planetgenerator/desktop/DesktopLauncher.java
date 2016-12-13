5c9457c11ef723e5de0537b5cf4b665f75bbcafe
Benjamin McLean <mclean.ben@gmail.com>
2016-12-12 21:29:38 -0600

    made new project

---
+++ desktop/src/net/benmclean/planetgenerator/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.benmclean.planetgenerator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.benmclean.planetgenerator.PlanetGeneratorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlanetGeneratorGame(), config);
+	}
+}
