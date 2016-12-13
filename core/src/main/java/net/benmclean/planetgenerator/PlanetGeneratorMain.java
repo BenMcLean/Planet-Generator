463d03891d12c037a67ad0bee99d9a6ce9ff507c
Benjamin McLean <mclean.ben@gmail.com>
2016-12-12 20:58:16 -0600

    deleted everything again

--- core/src/main/java/net/benmclean/planetgenerator/PlanetGeneratorMain.java
+++
@@ -1,7 +0,0 @@
-package net.benmclean.planetgenerator;
-
-import com.badlogic.gdx.ApplicationAdapter;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class PlanetGeneratorMain extends ApplicationAdapter {
-}
