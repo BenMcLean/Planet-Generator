6f489de64d0430bcd096f27dcbf4e1c5a2575d0c
Benjamin McLean <mclean.ben@gmail.com>
2016-12-21 11:55:20 -0600

    Added some bricks

--- android/assets-raw/magicktile.bat
+++ android/assets-raw/magicktile.bat
@@ -1,3 +1,4 @@
+rem This script will chop up images containing 16x16 tiles using http://www.graphicsmagick.org/
 gm convert %1 -map %1 -crop 16x16 +adjoin %%03d.png
 @echo off
 rem magick -define colorspace:auto-grayscale=off %1 -crop 16x16 -depth 2 -colors 4 %1
