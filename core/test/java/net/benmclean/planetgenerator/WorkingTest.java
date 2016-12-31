a8b3a027b9d030bd3243e93f90108c86553556dc
Benjamin McLean <mclean.ben@gmail.com>
2016-12-31 12:56:33 -0600

    Added a jUnit test
    Based on
    http://shahmirj.com/blog/getting-junit-working-with-libgdx-in-gradle

---
+++ core/test/java/net/benmclean/planetgenerator/WorkingTest.java
@@ -0,0 +1,18 @@
+package net.benmclean.planetgenerator;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class WorkingTest {
+    @Test
+    public void thisAlwaysPasses() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
