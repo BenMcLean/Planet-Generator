6b3c97287748d38600c5aabb17044cd9ac53120f
Benjamin McLean <mclean.ben@gmail.com>
2017-09-24 19:15:14 -0500

    added color manipulating script

--- desktop/src/net/benmclean/planetgenerator/desktop/ArtProcess.java
+++ desktop/src/net/benmclean/planetgenerator/desktop/ArtProcess.java
@@ -4,6 +4,11 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
+import net.benmclean.utils.Palette4;
 
 public class ArtProcess {
     public static void main (String[] args) {
@@ -14,6 +19,27 @@ public class ArtProcess {
     public static class ArtProcessGame extends Game {
         @Override
         public void create() {
+
+            pixmap.setBlending(Pixmap.Blending.None);
+
+            Palette4 palette = Palette4.gameboy();
+            Color color = Color.WHITE;
+            Color newColor = Color.WHITE;
+
+            for (int x = 0; x < pixmap.getWidth(); x++) {
+                for (int y = 0; y < pixmap.getHeight(); y++) {
+                    color.set(pixmap.getPixel(x, y));
+                    newColor.set(palette.getPixel((int) (color.r * 3.9999)));
+                    pixmap.drawPixel(x, y, Color.rgba8888(newColor.r, newColor.g, newColor.b, color.a));
+                }
+            }
+
+            FileHandle resultFile = Gdx.files.local("../assets-raw/utils/result.png");
+
+            PixmapIO.writePNG(resultFile, pixmap);
+
+            pixmap.dispose();
+
             System.out.println("Everything OK so far!");
             Gdx.app.exit();
         }
